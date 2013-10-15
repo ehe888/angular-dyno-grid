cad9109c9ecf0de42543178dbcff86f4ea0b7b6f
Valeriy <valeriyr82@gmail.com>
2013-10-15 11:30:33 +0300

    add hr-layout

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -19,7 +19,44 @@ angular.module('myApp.controllers', []).
   }).
   controller('MyCtrl1', function ($scope) {
     // write Ctrl here
-
+        $scope.layoutOptions = {
+            data: [3, 4]
+        }
+        $scope.layoutOptions2 = {
+            data: [['span3','span9'], [ 'span6', 'span6']]
+        }
+        $scope.layoutOptions3 = {
+            data: [
+                ['span9', { w: 'span3', d: [1, 1, 1]}],
+                [ 'span4', 'span4', 'span2', 'span2'],
+                3
+            ]
+        }
+        $scope.layoutOptions4 = {
+            data: [
+                ['span3','span9'],
+                [
+                    'span3',
+                    'span3',
+                    {
+                        w: 'span6',
+                        d: [
+                            2,
+                            [
+                                {
+                                    w:'span9',
+                                    d: [
+                                        3,
+                                        ['span7','span5']
+                                    ]
+                                },
+                                'span3'
+                            ]
+                        ]
+                    }
+                ]
+            ]
+        }
   }).
   controller('MyCtrl2', function ($scope) {
     // write Ctrl here
--- public/js/directives.js
+++ public/js/directives.js
@@ -8,3 +8,61 @@ angular.module('myApp.directives', []).
       elm.text(version);
     };
   });
+
+angular.module("myApp.directives", []).directive("hrLayout", [
+    "$compile", "$q", "$parse", function($compile, $q, $parse) {
+        return {
+            restrict: "A",
+            priority: 10001,
+            scope: true,
+            controller: [
+                "$scope", "$timeout", function($scope, $timeout) {
+                    console.log('controller', $scope.params);
+                }
+            ],
+            compile: function(element, attrs) {
+                return function(scope, element, attrs) {
+
+                    var renderLayout = function(values) {
+                        var renderedHTML = '';
+                        var rowCnt = 0;
+                        var subWidth = 0;
+                        angular.forEach(values, function(value) {
+                            renderedHTML += '<div class="row">';
+                            if(Object.prototype.toString.call(value) === '[object Array]') {
+                                angular.forEach(value, function(subvalue) {
+                                    if(typeof subvalue === 'object') {
+                                        renderedHTML += '<div class="col-md-'+subvalue.w.substring(4)+'">';
+                                        renderedHTML += renderLayout(subvalue.d);
+                                        renderedHTML += '</div>';
+                                    } else {
+                                        renderedHTML += '<div class="col-md-'+subvalue.substring(4)+'">Test content(col-'+subvalue.substring(4)+')</div>';
+                                    }
+                                });
+                            } else {
+                                if(value > 12) {
+                                    value = 12;
+                                } else if (value <= 0) {
+                                    value = 1;
+                                }
+                                subWidth = Math.floor(12 / value);
+                                for (var i=0; i< value-1; i++) {
+                                    renderedHTML += '<div class="col-md-'+subWidth+'">Test content(col-'+subWidth+')</div>';
+                                }
+                                renderedHTML += '<div class="col-md-'+(12-subWidth*(value-1))+'">Test content(col-'+(12-subWidth*(value-1))+')</div>';
+                            }
+                            renderedHTML += '</div>';
+                            rowCnt++;
+                        });
+                        return renderedHTML;
+                    }
+
+                    scope.$watch(attrs.hrLayout, function(value) {
+                        element.html(renderLayout(value.data));
+                    });
+                    element.addClass("hr-layout");
+                };
+            }
+        };
+    }
+]);
